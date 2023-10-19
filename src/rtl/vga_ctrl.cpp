9bf23db0c7bc0322002cf6dedfeb83585d60717f
timemeansalot <timemeansalot@gmail.com>
2023-10-19 10:50:00 +0800

    refactor vc test

--- src/rtl/vga_ctrl.cpp
+++ src/rtl/vga_ctrl.cpp
@@ -2,41 +2,31 @@
 #include "config.h"
 #include <cstdio>
 
-void vga_ctrl::set_resolution(int hsync_end_i, int hpulse_end_i,
-                              int hdata_begin_i, int hdata_end_i,
-                              int vsync_end_i, int vpulse_end_i,
-                              int vdata_begin_i, int vdata_end_i) {
-  this->hsync_end_i = hsync_end_i;
-  this->hpulse_end_i = hpulse_end_i;
-  this->hdata_begin_i = hdata_begin_i;
-  this->hdata_end_i = hdata_end_i;
-  this->vsync_end_i = vsync_end_i;
-  this->vpulse_end_i = vpulse_end_i;
-  this->vdata_begin_i = vdata_begin_i;
-  this->vdata_end_i = vdata_end_i;
-}
-
-void vga_ctrl::eval(int data_i, int resetn) {
+void vga_ctrl::eval() {
 
   // calculate output color
-  this->data_i = data_i;
-  red_o = data_i & 0xf;
-  green_o = (data_i >> 4) & 0xf;
-  blue_o = (data_i >> 8) & 0xf;
+  out->red_o = in->data_i & 0xf;
+  out->green_o = (in->data_i >> 4) & 0xf;
+  out->blue_o = (in->data_i >> 8) & 0xf;
 
   // calculate sync signal
-  if (resetn == 0) {
-    vcount = 0;
-    hcount = 0;
-  } else {
-    vcount += 1;
-    hcount += 1;
+  if (in->clk) { // only eval at posedge
+    if (in->resetn == 0) {
+      vcount = 0;
+      hcount = 0;
+    } else {
+      vcount += 1;
+      hcount += 1;
+    }
   }
-  hsync_o = hcount <= hpulse_end_i ? 0 : 1;
-  vsync_o = vcount <= vpulse_end_i ? 0 : 1;
-  blank_o = ((hcount >= hdata_begin_i - 1) && (hcount <= hdata_end_i - 1)) &&
-            ((vcount >= vdata_begin_i - 1) && (vcount <= hdata_end_i - 1));
-  data_req_o = blank_o; //TODO: make sure data_req_o and blank_o is correct in cycle
+  // calculate sync data
+  out->hsync_o = hcount <= in->hpulse_end_i ? 0 : 1;
+  out->vsync_o = vcount <= in->vpulse_end_i ? 0 : 1;
+  out->blank_o =
+      ((hcount >= in->hdata_begin_i - 1) && (hcount <= in->hdata_end_i - 1)) &&
+      ((vcount >= in->vdata_begin_i - 1) && (vcount <= in->hdata_end_i - 1));
+  out->data_req_o = out->blank_o; // TODO: make sure data_req_o and blank_o is
+                                  // correct in cycle
 
   Log("hcount=%d, vcount=%d\n", hcount, vcount);
   Log("hsync_end_i=%d\n", hsync_end_i);
--- src/rtl/vga_ctrl.h
+++ src/rtl/vga_ctrl.h
@@ -1,9 +1,9 @@
 #ifndef __VGA_CTRL__
 #define __VGA_CTRL__
-class vga_ctrl {
-
-private:
-  // resolotion configs
+class vc_in_io {
+public:
+  bool clk, resetn;
+  // ConfigUnit input
   int hsync_end_i;
   int hpulse_end_i;
   int hdata_begin_i;
@@ -12,19 +12,32 @@ private:
   int vpulse_end_i;
   int vdata_begin_i;
   int vdata_end_i;
-
-  int vcount, hcount;
-
-public:
+  // PingPongRegister input
   int data_i;
+  // get random input
+  void randInIO(unsigned long int sim_time);
+};
+class vc_out_io {
+public:
   int red_o, green_o, blue_o;
   int vsync_o, hsync_o, blank_o;
   bool data_req_o;
+};
+class vga_ctrl {
 
-  void set_resolution(int hsync_end_i, int hpulse_end_i, int hdata_begin_i,
-                      int hdata_end_i, int vsync_end_i, int vpulse_end_i,
-                      int vdata_begin_i, int vdata_end_i);
+private:
+  // resolotion configs
+  int vcount, hcount;
 
-  void eval(int data_i, int resetn); // dut step
+public:
+  vc_in_io *in;
+  vc_out_io *out;
+
+  void resetn(); // resetn c_model
+  void eval();   // step one cycle
+  vga_ctrl() {
+    in = new vc_in_io;
+    out = new vc_out_io;
+  }
 };
 #endif
