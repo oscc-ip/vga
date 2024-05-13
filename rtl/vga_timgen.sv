8584fb7fa4ad179bd7945ba99a76bb80adf21c6e
maksyuki <maksyuki@126.com>
2024-05-13 10:32:26 +0800

    feat: inst vga cnt in vga tgmen module

--- rtl/vga_timgen.sv
+++ rtl/vga_timgen.sv
@@ -15,15 +15,48 @@ module vga_timgen (
     input  logic                     clk_i,
     input  logic                     rst_n_i,
     input  logic                     en_i,
-    input  logic [`VGA_TB_WIDTH-1:0] bpsize_i,
-    input  logic [`VGA_TB_WIDTH-1:0] snsize_i,
-    input  logic [`VGA_TB_WIDTH-1:0] fpsize_i,
-    input  logic [`VGA_VB_WIDTH-1:0] vlen_i,
-    output logic                     end_o,
-    output logic                     vis_o,
-    output logic                     sync_o
+    input  logic [`VGA_TB_WIDTH-1:0] hbpsize_i,
+    input  logic [`VGA_TB_WIDTH-1:0] hsnsize_i,
+    input  logic [`VGA_TB_WIDTH-1:0] hfpsize_i,
+    input  logic [`VGA_VB_WIDTH-1:0] hvlen_i,
+    input  logic [`VGA_TB_WIDTH-1:0] vbpsize_i,
+    input  logic [`VGA_TB_WIDTH-1:0] vsnsize_i,
+    input  logic [`VGA_TB_WIDTH-1:0] vfpsize_i,
+    input  logic [`VGA_VB_WIDTH-1:0] vvlen_i,
+    output logic                     hsync_o,
+    output logic                     hend_o,
+    output logic                     vsync_o,
+    output logic                     vend_o,
+    output logic                     de_o
 );
 
+  logic s_hvis, s_vvis;
+  assign de_o = s_hvis && s_vvis;
 
+  vga_cnt u_hori_vga_cnt (
+      .clk_i   (clk_i),
+      .rst_n_i (rst_n_i),
+      .en_i    (en_i),
+      .bpsize_i(hbpsize_i),
+      .snsize_i(hsnsize_i),
+      .fpsize_i(hfpsize_i),
+      .vlen_i  (hvlen_i),
+      .vis_o   (s_hvis),
+      .sync_o  (hsync_o),
+      .end_o   (hend_o)
+  );
+
+  vga_cnt u_vert_vga_cnt (
+      .clk_i   (clk_i),
+      .rst_n_i (rst_n_i),
+      .en_i    (en_i && hend_o),
+      .bpsize_i(vbpsize_i),
+      .snsize_i(vsnsize_i),
+      .fpsize_i(vfpsize_i),
+      .vlen_i  (vvlen_i),
+      .vis_o   (s_vvis),
+      .sync_o  (vsync_o),
+      .end_o   (vend_o)
+  );
 
 endmodule
