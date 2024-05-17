08db57cefda4ef7632691433341a30bf5eff4b0e
maksyuki <maksyuki@126.com>
2024-05-17 10:16:27 +0800

    fix: fix same slave reg addr bug

--- rtl/vga_define.sv
+++ rtl/vga_define.sv
@@ -1,3 +1,42 @@
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+////  WISHBONE rev.B2 compliant enhanced VGA/LCD Core            ////
+////  Wishbone slave interface                                   ////
+////                                                             ////
+////  Author: Richard Herveille                                  ////
+////          richard@asics.ws                                   ////
+////          www.asics.ws                                       ////
+////                                                             ////
+////  Downloaded from: http://www.opencores.org/projects/vga_lcd ////
+////                                                             ////
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+//// Copyright (C) 2001, 2002 Richard Herveille                  ////
+////                    richard@asics.ws                         ////
+////                                                             ////
+//// This source file may be used and distributed without        ////
+//// restriction provided that this copyright statement is not   ////
+//// removed from the file and that any derivative work contains ////
+//// the original copyright notice and the associated disclaimer.////
+////                                                             ////
+////     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     ////
+//// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   ////
+//// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   ////
+//// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      ////
+//// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         ////
+//// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    ////
+//// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   ////
+//// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        ////
+//// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  ////
+//// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  ////
+//// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  ////
+//// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         ////
+//// POSSIBILITY OF SUCH DAMAGE.                                 ////
+////                                                             ////
+/////////////////////////////////////////////////////////////////////
+//
+// -- Adaptable modifications are redistributed under compatible License --
+//
 // Copyright (c) 2023 Beijing Institute of Open Source Chip
 // vga is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan PSL v2.
@@ -61,8 +100,8 @@
 `define VGA_HTIM  4'b0010 // BASEADDR + 0x08
 `define VGA_VTIM  4'b0011 // BASEADDR + 0x0C
 `define VGA_FBBA1 4'b0100 // BASEADDR + 0x10
-`define VGA_FBBA2 4'b0100 // BASEADDR + 0x14
-`define VGA_STAT  4'b0100 // BASEADDR + 0x18
+`define VGA_FBBA2 4'b0101 // BASEADDR + 0x14
+`define VGA_STAT  4'b0110 // BASEADDR + 0x18
 
 
 `define VGA_CTRL_ADDR  {26'b0, `VGA_CTRL , 2'b00}
